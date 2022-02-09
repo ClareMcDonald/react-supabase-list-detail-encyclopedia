9bfaaad23437cf9ee46484887495f2414b2a942b
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-08 18:00:27 -0800

    work on PokemonDetail

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useEffect, useState } from 'react-router-dom';
-import {}
+import { getSinglePokemon } from './services/fetch-utils';
 
 export default function PokemonDetail() {
+  const [poke, setPoke] = useState({});
   return (
     <div>PokemonDetail</div>
   );
