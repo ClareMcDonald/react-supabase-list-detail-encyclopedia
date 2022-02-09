8d4ded15e68ee1e88491bef19410a19a9303d7b9
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-08 17:38:14 -0800

    pokemon list displays

--- src/SinglePokemon.js
+++ src/SinglePokemon.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './App.css';
 
 export default function SinglePokemon({ poke }) {
   return (
     <Link to={`/pokemon/${poke.id}`} className="poke">
-      {poke.name}
+      {poke.pokemon}
+      <img src={poke.url_image}/>
     </Link>
   );
 }
