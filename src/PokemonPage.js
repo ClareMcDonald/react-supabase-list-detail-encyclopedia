0eaae85d058e6c3acdeb34c81e116c02aba591ff
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-08 16:31:16 -0800

    wrote useEffect in PokemonPage

--- src/PokemonPage.js
+++ src/PokemonPage.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPokemon } from './fetch-utils';
 
 export default function PokemonPage() {
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 20;
+
+  useEffect(() => {
+    async function fetchPokes() {
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      const pokes = await getPokemon(from, to);
+
+      setPokemon(pokes);
+    }
+
+    fetchPokes();
+
+  }, [page]);
+    
   return (
     <>
+      <button>Previous</button>
+      <button>Next</button>
       <PokemonList />
     </>
   );
