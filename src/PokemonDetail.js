24694c403d787f0078076efc6677477ee94f2db6
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-09 21:08:19 -0800

    wrote return of PokemonDetail

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getSinglePokemon } from './services/fetch-utils';
 
-export default function PokemonDetail({ poke }) {
+export default function PokemonDetail() {
   const [pokemon, setPokemon] = useState({});
   const params = useParams();
 
@@ -21,12 +21,23 @@ export default function PokemonDetail({ poke }) {
   function handlePokemonClick() {
     window.location.replace('/pokemon/${params.id}');
   }
-  
+
   return (
     <>
       <Link to="/">Home</Link>
-
-      <div>PokemonDetail</div>
+      <div className="pokemon-detail" onClick={handlePokemonClick}>
+        <div>Name: {pokemon.pokemon}</div>
+        <img src={pokemon.url_image}/>
+        <p>Type 1: {pokemon.type_1}</p>
+        <p>Type 2: {pokemon.type_2}</p>
+        <p>Height: {pokemon.height}</p>
+        <p>Weight: {pokemon.weight}</p>
+        <p>Attack: {pokemon.attack}</p>
+        <p>Defense: {pokemon.defense}</p>
+        <p>HP: {pokemon.hp}</p>
+        <p>Speed:{pokemon.speed}</p>
+      </div>
     </>
   );
 }
+
